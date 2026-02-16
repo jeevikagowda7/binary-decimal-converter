a3bec013c42427f28023aebd0690668d077658c7
jeevikagowda7 <anjeevikagowda7@gmail.com>
2026-02-16 17:41:54 +0530

    Add files via upload
    Added input validation to ensure only binary digits are accepted

--- code1.m
+++ code1.m
@@ -1,6 +1,9 @@
 binary = input('Enter a binary number: ', 's');
 decimal = 0;
 length_bin = length(binary);
+if any(binary ~= '0' & binary ~= '1')
+    error('Input must be a binary number containing only 0s and 1s.');
+end
 
 for i = 1:length_bin
     bit = str2double(binary(i));
